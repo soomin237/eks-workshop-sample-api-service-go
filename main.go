445dc97fbab47fdc766c66ab72413c930f451a42
Woo Hyung Choi <whchoi98@gmail.com>
2023-01-31 16:10:44 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello K8s World"}
+		res := &response{Message: "Hello K8s K8s K8s K8s K8s K8s K8s K8s World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
