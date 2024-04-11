1364305f8e13002f94620ec45e9377b26659357e
Woo Hyung Choi <whchoi98@gmail.com>
2024-04-11 15:28:46 +0900

    codepipeline test 20240411

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello K8s Jenkins111 World"}
+		res := &response{Message: "Hello K8s Codepipeline test 2024-04-11 World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
